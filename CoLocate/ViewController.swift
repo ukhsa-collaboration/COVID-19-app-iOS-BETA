d64c2a8f664062f9a3d9b65824058623f640d0ee
Adam Fowler <adam@adamfowler.org>
2020-03-09 09:19:56 +0000

    Initial Commit

---
+++ CoLocate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoLocate
+//
+//  Created by NHSX.
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
