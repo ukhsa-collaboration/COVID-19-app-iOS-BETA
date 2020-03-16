9ef972cd0805e37c3e2e11f172d3ad4c2165650b
Robert Atkins <ratkins@pivotal.io>
2020-03-16 15:49:55 +0100

    Give logo strapline an intrinsic height

--- CoLocate/LogoStrapline.swift
+++ CoLocate/LogoStrapline.swift
@@ -9,43 +9,49 @@ import UIKit
 
 @IBDesignable
 class LogoStrapline: UIView {
-  @IBOutlet var containerView: UIView!
-  @IBOutlet var titleLabel: UILabel!
-
-  static var nibName: String {
-    String(describing: self)
-  }
-
-  required init?(coder aDecoder: NSCoder) {
-    super.init(coder: aDecoder)
-    commonInit()
-  }
-
-  override init(frame: CGRect) {
-    super.init(frame: frame)
-    commonInit()
-  }
-
-  override func prepareForInterfaceBuilder() {
-    super.prepareForInterfaceBuilder()
-    commonInit()
-  }
-
-  func commonInit() {
-    guard let view = loadViewFromNib() else {
-      return
+    
+    @IBOutlet var containerView: UIView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 64.0)
     }
-
-    view.backgroundColor = .nhsBlue
-    titleLabel.textColor = .white
-    titleLabel.text = "Coronavirus tracing"
-
-    view.frame = self.bounds
-    self.addSubview(view)
-  }
-
-  func loadViewFromNib() -> UIView? {
-    let nib = UINib(nibName: LogoStrapline.nibName, bundle: Bundle(for: LogoStrapline.self))
-    return nib.instantiate(withOwner: self, options: nil).first as? UIView
-  }
+    
+    static var nibName: String {
+        String(describing: self)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        commonInit()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        commonInit()
+    }
+    
+    func commonInit() {
+        guard let view = loadViewFromNib() else {
+            return
+        }
+        
+        view.backgroundColor = .nhsBlue
+        titleLabel.textColor = .white
+        titleLabel.text = "Coronavirus tracing"
+        
+        view.frame = self.bounds
+        self.addSubview(view)
+    }
+    
+    func loadViewFromNib() -> UIView? {
+        let nib = UINib(nibName: LogoStrapline.nibName, bundle: Bundle(for: LogoStrapline.self))
+        return nib.instantiate(withOwner: self, options: nil).first as? UIView
+    }
+    
 }
