902c5008ba13d70237d3e659dafae07e9bd104d6
Tim Jarratt <tjarratt@pivotal.io>
2020-03-21 11:17:37 +0100

    Backfill more tests for Diagnosis Serrvice

--- CoLocateTests/DiagnosisServiceTests.swift
+++ CoLocateTests/DiagnosisServiceTests.swift
@@ -16,4 +16,22 @@ class DiagnosisServiceTests: XCTestCase {
         XCTAssertEqual(Diagnosis(rawValue: 0), Diagnosis.unknown)
     }
 
+    func testDiagnosisIsPersisted() {
+        let service = DiagnosisService()
+        service.recordDiagnosis(Diagnosis.notInfected)
+
+        let diagnosis = DiagnosisService().currentDiagnosis
+        XCTAssertEqual(diagnosis, Diagnosis.notInfected)
+    }
+
+    func testDiagnosisIsUnknownWhenDefaultsReset() {
+        let service = DiagnosisService()
+        service.recordDiagnosis(Diagnosis.infected)
+
+        let appDomain = Bundle.main.bundleIdentifier
+        UserDefaults.standard.removePersistentDomain(forName: appDomain!)
+
+        let diagnosis = service.currentDiagnosis
+        XCTAssertEqual(diagnosis, Diagnosis.unknown)
+    }
 }
