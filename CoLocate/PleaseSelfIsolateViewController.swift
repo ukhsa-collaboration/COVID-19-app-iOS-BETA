65460a55ab7e2ed467095e5a42b26c4d0dbf6801
Robert Atkins <ratkins@pivotal.io>
2020-03-21 14:15:32 +0100

    Cleanup "Please Isolate" view controller
    Signed-off-by: Tim Jarratt <tjarratt@pivotal.io>

--- CoLocate/PleaseSelfIsolateViewController.swift
+++ CoLocate/PleaseSelfIsolateViewController.swift
@@ -12,7 +12,6 @@ class PleaseSelfIsolateViewController: UIViewController {
 
     @IBOutlet weak var warningView: UIView!
     @IBOutlet weak var warningViewTitle: UILabel!
-//    @IBOutlet weak var warningViewBody: UILabel!
     @IBOutlet weak var shareDiagnosisTitle: UILabel!
     @IBOutlet weak var shareDiagnosisBody: UILabel!
     @IBOutlet weak var shareDiagnosisButton: PrimaryButton!
@@ -33,16 +32,4 @@ class PleaseSelfIsolateViewController: UIViewController {
         moreInformationTitle.text = "Steps you can take"
         moreInformationBody.text = "Based on people you've been in contact with, you may have been exposted to coronavirus.\n\nIf you're on public transport, go home by the most direct route. Stay at least 2 meters away from people if you can. If you're at home:\n\n• Find a room where you can close the door\n\n• Avoid touching people, surfaces and objects\n\nTo keep other people safe, don't visit your GP, pharmacy or hospital"
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
