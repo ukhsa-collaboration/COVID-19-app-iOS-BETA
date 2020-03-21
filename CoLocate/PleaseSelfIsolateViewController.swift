03d980dcfcb632232468439817d8a1a40f540b1a
Tim Jarratt <tjarratt@pivotal.io>
2020-03-21 15:29:32 +0100

    Users can submit their historical contact events
    [#171833932]

--- CoLocate/PleaseSelfIsolateViewController.swift
+++ CoLocate/PleaseSelfIsolateViewController.swift
@@ -18,6 +18,7 @@ class PleaseSelfIsolateViewController: UIViewController {
     @IBOutlet weak var moreInformationTitle: UILabel!
     @IBOutlet weak var moreInformationBody: UILabel!
 
+    let contactEventRecorder: ContactEventRecorder = PlistContactEventRecorder.shared
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,5 +32,48 @@ class PleaseSelfIsolateViewController: UIViewController {
         shareDiagnosisButton.setTitle("Notify", for: .normal)
         moreInformationTitle.text = "Steps you can take"
         moreInformationBody.text = "Based on people you've been in contact with, you may have been exposted to coronavirus.\n\nIf you're on public transport, go home by the most direct route. Stay at least 2 meters away from people if you can. If you're at home:\n\n• Find a room where you can close the door\n\n• Avoid touching people, surfaces and objects\n\nTo keep other people safe, don't visit your GP, pharmacy or hospital"
+    }
+
+    @IBAction func didTapNotify(_ sender: Any) {
+        let deviceId = UIDevice.current.identifierForVendor!
+        let contactEvents = contactEventRecorder.contactEvents
+        let request = PatchContactEventsRequest(deviceId: deviceId, contactEvents: contactEvents)
+
+        URLSession.shared.execute(request) { (result) in
+            switch result {
+            case .success(_):
+                print("\(#file).\(#function) SUCCESS !")
+
+                DispatchQueue.main.async {
+                    self.present(self.successfulAlert(), animated: true, completion: nil)
+                }
+            case .failure(let error):
+                print("\(#file).\(#function) failure submitting contact events (\(error)) !")
+
+                DispatchQueue.main.async {
+                    self.present(self.errorAlert(), animated: true, completion: nil)
+                }
+            }
+        }
+    }
+
+    // MARK: - Alert Controllers
+
+    func successfulAlert() -> UIAlertController {
+        let alert = UIAlertController(title: NSLocalizedString("Thank you", comment: ""), message: NSLocalizedString("Thank you for sharing your anonymized contact information with us. We will use this information to help inform anyone that may have been in close contact with you recently.", comment: ""), preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+            alert.dismiss(animated: true)
+        }))
+
+        return alert
+    }
+
+    func errorAlert() -> UIAlertController {
+        let alert = UIAlertController(title: NSLocalizedString("Error", comment: ""), message: NSLocalizedString("We're sorry, your request could not be handled at this time. Please try again later, so that we can help alert and protect people that you have been in close contact recently", comment: ""), preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default, handler: { _ in
+            alert.dismiss(animated: true)
+        }))
+
+        return alert
     }
 }
