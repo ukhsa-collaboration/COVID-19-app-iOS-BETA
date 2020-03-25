5af3da6b32c2526a36181141981bd82ce4b843bc
Robert Atkins <ratkins@pivotal.io>
2020-03-25 12:30:17 +0100

    Bring back Alina's changes
    Signed-off-by: Tim Jarratt <tjarratt@pivotal.io>

---
+++ CoLocate/Extensions/String+localized.swift
@@ -0,0 +1,15 @@
+//
+//  String+localized.swift
+//  CoLocate
+//
+//  Created by NHSX.
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import Foundation
+
+extension String {
+    var localized: String {
+        return NSLocalizedString(self, tableName: nil, bundle: Bundle.main, value: "", comment: "")
+    }
+}
