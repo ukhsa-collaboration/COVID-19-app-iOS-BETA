559fd35c11c2bf7f7a2ba3d5edce09ab3fb0d5c5
Anon 12 <anon12@example.com>
2020-03-24 17:57:40 -0700

    Removed dead code

--- CoLocate/AppDelegate.swift
+++ CoLocate/AppDelegate.swift
@@ -14,8 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    var broadcaster: BTLEBroadcaster?
-    var listener: BTLEListener?
     var diagnosisService: DiagnosisService = DiagnosisService()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
