63d693f2483a5a93e136e39b906cfa47877a5a42
Tim Jarratt <tjarratt@pivotal.io>
2020-03-26 19:21:01 +0100

    Remove dummy key and uuid from requests

--- CoLocate/Networking/RequestFactory.swift
+++ CoLocate/Networking/RequestFactory.swift
@@ -11,27 +11,25 @@ import CryptoKit
 
 struct RequestFactory {
 
-    // This uuid is pre-seeded on the database (for now)
-    // this won't work if the database is dropped and recreated
-    // this will change once we can store and read the deviceId we receive during registration
-    static let shared = RequestFactory(deviceId: UUID(uuidString: "ba64976f-e2f8-4841-b505-e3a3c1dd820d")!)
-    
+    static let shared = RequestFactory(registrationStorage: SecureRegistrationStorage.shared)
     static let pushToken = "this should be the base64-encoded push token we get from firebase=="
 
-    // TODO: this needs to be read from the keychain
-    let dummyKey: SymmetricKey = SymmetricKey(data: Data(base64Encoded: "3bLIKs9B9UqVfqGatyJbiRGNW8zTBr2tgxYJh/el7pc=")!)
-
-    let deviceId: UUID
-    
     static func registrationRequest() -> RegistrationRequest {
         return RegistrationRequest(pushToken: pushToken)
     }
 
-    init(deviceId: UUID) {
-        self.deviceId = deviceId
+    private let registrationStorage: SecureRegistrationStorage
+
+    init(registrationStorage: SecureRegistrationStorage) {
+        self.registrationStorage = registrationStorage
     }
 
     func patchContactsRequest(contactEvents: [ContactEvent]) -> PatchContactEventsRequest {
-        return PatchContactEventsRequest(key: dummyKey, deviceId: deviceId, contactEvents: contactEvents)
+        let registration = try! registrationStorage.get()!
+
+        let deviceId = registration.id
+        let symmetricKey = SymmetricKey(data: registration.secretKey.data(using: .utf8)!)
+
+        return PatchContactEventsRequest(key: symmetricKey, deviceId: deviceId, contactEvents: contactEvents)
     }
 }
