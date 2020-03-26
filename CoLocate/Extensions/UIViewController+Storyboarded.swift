7cdd8aee5d75d4834a5f2efe0bcc9b573416ae3c
Anon 20 <anon20@example.com>
2020-03-26 10:24:33 +0000

    added extension for storyboarding a uiviewcontroller

---
+++ CoLocate/Extensions/UIViewController+Storyboarded.swift
@@ -0,0 +1,31 @@
+//
+//  UIViewController+Storyboarded.swift
+//  CoLocate
+//
+//  Created by NHSX.
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import UIKit
+
+enum Storyboard: String {
+    case okNow = "OkNow"
+    case permissions = "Permissions"
+    case enterDiagnosis = "EnterDiagnosis"
+    case pleaseSelfIsolate = "PleaseSelfIsolate"
+}
+
+protocol Storyboarded: class {
+    static func instantiate(storyboard: Storyboard) -> Self
+}
+
+extension Storyboarded where Self: UIViewController {
+    static func instantiate(storyboard: Storyboard = .okNow) -> Self {
+   
+        let fullName = NSStringFromClass(self)
+        let className = fullName.components(separatedBy: ".")[1]
+        let storyboard = UIStoryboard(name: storyboard.rawValue, bundle: Bundle.main)
+        return storyboard.instantiateViewController(withIdentifier: className) as! Self
+    }
+}
+
