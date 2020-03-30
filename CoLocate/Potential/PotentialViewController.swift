15b8bfbaf134be7a468612f648ad54efe38c8c7b
Alpha Chen <achen@pivotal.io>
2020-03-30 16:38:32 -0700

    Show more info on the potential screen
    Signed-off-by: Alpha Chen <achen@pivotal.io>

--- CoLocate/Potential/PotentialViewController.swift
+++ CoLocate/Potential/PotentialViewController.swift
@@ -9,5 +9,25 @@
 import UIKit
 
 class PotentialViewController: UIViewController, Storyboarded {
+
     static let storyboardName = "Potential"
+
+    @IBOutlet weak var label: UILabel!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.isNavigationBarHidden = true
+
+        let url = Bundle.main.url(forResource: "Potential", withExtension: "rtf")!
+        label.attributedText = try! NSAttributedString(
+            url: url,
+            options: [.documentType: NSAttributedString.DocumentType.rtf],
+            documentAttributes: nil
+        )
+    }
+
+    @IBAction func resultCodeTapped(_ sender: PrimaryButton) {
+    }
+
 }
