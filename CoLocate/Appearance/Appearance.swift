9f077ab00165c323239526e1361039642ebed20b
Alpha Chen <achen@pivotal.io>
2020-04-13 15:12:33 -0700

    Some accessibility fixes for the status screen

--- CoLocate/Appearance/Appearance.swift
+++ CoLocate/Appearance/Appearance.swift
@@ -13,6 +13,7 @@ extension Appearance {
     static func setup() {
         UILabel.appearance().textColor = UIColor(named: "NHS Text")
         UILabel.appearance().adjustsFontForContentSizeCategory = true
+        UILabel.appearance().numberOfLines = 0
 
         UILabel.appearance(whenContainedInInstancesOf: [LogoStrapline.self]).textColor = UIColor(named: "NHS White")
     }
