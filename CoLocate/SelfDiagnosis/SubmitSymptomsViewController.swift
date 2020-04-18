909b230e1634b250efbd24ade4b2612cda4aee71
Alpha Chen <achen@pivotal.io>
2020-04-17 17:31:42 -0700

    Rearrange code in submit symptoms view controller

--- CoLocate/SelfDiagnosis/SubmitSymptomsViewController.swift
+++ CoLocate/SelfDiagnosis/SubmitSymptomsViewController.swift
@@ -12,22 +12,14 @@ import Logging
 class SubmitSymptomsViewController: UIViewController, Storyboarded {
     static let storyboardName = "SelfDiagnosis"
 
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
-    @IBOutlet weak var startDateView: UIView!
-    @IBOutlet weak var thankYouLabel: UILabel!
-    @IBOutlet weak var submitButton: PrimaryButton!
-    var startDateViewController: StartDateViewController!
+    // MARK: - Dependencies
 
     private var persisting: Persisting!
     private var contactEventRepository: ContactEventRepository!
     private var session: Session!
     private var notificationCenter: NotificationCenter!
-
     private var symptoms: Set<Symptom>!
-    private var startDate: Date?
-    private var isSubmitting = false
-    
+
     func inject(
         persisting: Persisting,
         contactEventRepository: ContactEventRepository,
@@ -49,7 +41,28 @@ class SubmitSymptomsViewController: UIViewController, Storyboarded {
             symptoms.insert(.cough)
         }
     }
-    
+
+    // MARK: - UIKit
+
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var heightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var startDateView: UIView!
+    @IBOutlet weak var thankYouLabel: UILabel!
+    @IBOutlet weak var submitButton: PrimaryButton!
+
+    var startDateViewController: StartDateViewController!
+    private var startDate: Date?
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.destination {
+        case let vc as StartDateViewController:
+            startDateViewController = vc
+            vc.inject(symptoms: symptoms, delegate: self)
+        default:
+            break
+        }
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -57,20 +70,10 @@ class SubmitSymptomsViewController: UIViewController, Storyboarded {
 
         thankYouLabel.text = "SUBMIT_SYMPTOMS_THANK_YOU".localized
 
-        notificationCenter.addObserver(
-            self,
-            selector: #selector(keyboardWillShow(notification:)),
-            name: UIResponder.keyboardWillShowNotification,
-            object: nil
-        )
-        notificationCenter.addObserver(
-            self,
-            selector: #selector(keyboardWillHide(notification:)),
-            name: UIResponder.keyboardWillHideNotification,
-            object: nil
-        )
+        addKeyboardObservers()
     }
 
+    private var isSubmitting = false
     @IBAction func submitTapped(_ sender: PrimaryButton) {
         guard let registration = persisting.registration else {
             fatalError("What do we do when we aren't registered?")
@@ -124,14 +127,32 @@ class SubmitSymptomsViewController: UIViewController, Storyboarded {
         }
     }
 
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        switch segue.destination {
-        case let vc as StartDateViewController:
-            startDateViewController = vc
-            vc.inject(symptoms: symptoms, delegate: self)
-        default:
-            break
-        }
+    private func alert(message: String) {
+        let alert = UIAlertController(
+            title: nil,
+            message: message,
+            preferredStyle: .alert
+        )
+
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        present(alert, animated: true)
+    }
+
+    // MARK: - Keyboard
+
+    private func addKeyboardObservers() {
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardWillShow(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil
+        )
+        notificationCenter.addObserver(
+            self,
+            selector: #selector(keyboardWillHide(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil
+        )
     }
 
     @objc func keyboardWillShow(notification: Notification) {
@@ -169,17 +190,6 @@ class SubmitSymptomsViewController: UIViewController, Storyboarded {
         })
     }
 
-    private func alert(message: String) {
-        let alert = UIAlertController(
-            title: nil,
-            message: message,
-            preferredStyle: .alert
-        )
-
-        alert.addAction(UIAlertAction(title: "OK", style: .default))
-        present(alert, animated: true)
-    }
-
     private func animate(withKeyboardNotification notification: Notification, animations: @escaping () -> (), completion: @escaping (Bool) -> () = { _ in }) {
         guard
             let kbDuration = notification.userInfo?[UIResponder.keyboardAnimationDurationUserInfoKey] as? TimeInterval,
@@ -196,8 +206,9 @@ class SubmitSymptomsViewController: UIViewController, Storyboarded {
             completion: completion
         )
     }
-
 }
+
+// MARK: - StartDateViewControllerDelegate
 
 extension SubmitSymptomsViewController: StartDateViewControllerDelegate {
     func startDateViewController(_ vc: StartDateViewController, didSelectDate date: Date) {
