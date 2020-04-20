3897e5504c53eda6699ea75fba5d6d88ede84e39
Anon 12 <anon12@example.com>
2020-04-20 11:14:03 -0700

    Test the output of BroadcastRotationKeyConverter.fromData in more depth

--- CoLocateTests/Bluetooth/BroadcastId/BroadcastRotationKeyConverterTests.swift
+++ CoLocateTests/Bluetooth/BroadcastId/BroadcastRotationKeyConverterTests.swift
@@ -19,22 +19,26 @@ class BroadcastRotationKeyConverterTests: XCTestCase {
     }
 
     func testConvertsKnownGoodBytesIntoAPublicKey() throws {
-        let testKeyData = ellipticCurveKeyForTest()
-
-        let key = try converter.fromData(testKeyData)
+        let input = Data(base64Encoded: "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEu1f68MqDXbKeTqZMTHsOGToO4rKnPClXe/kE+oWqlaWZQv4J1E98cUNdpzF9JIFRPMCNdGOvTr4UB+BhQv9GWg==")!
+        
+        let key = try converter.fromData(input)
 
         XCTAssertNotNil(key)
+        
+        var error: Unmanaged<CFError>?
+        guard let cfdata = SecKeyCopyExternalRepresentation(key, &error) else {
+            XCTFail("Could not extract key representation: \(String(describing: error))")
+            return
+        }
+        
+        let b64key = (cfdata as Data).base64EncodedString()
+        XCTAssertEqual(b64key, "BLtX+vDKg12ynk6mTEx7Dhk6DuKypzwpV3v5BPqFqpWlmUL+CdRPfHFDXacxfSSBUTzAjXRjr06+FAfgYUL/Rlo=")
+           let data:Data = cfdata as Data
+           let b64Key = data.base64EncodedString()
+            print("\(b64Key)")
     }
 
     func testThrowsWhenBytesDoNotRessembleAPublicKey() throws {
         XCTAssertThrowsError(try converter.fromData(Data()))
-    }
-
-    //MARK: - Private
-
-    private func ellipticCurveKeyForTest() -> Data {
-        let base64EncodedKey = "MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEu1f68MqDXbKeTqZMTHsOGToO4rKnPClXe/kE+oWqlaWZQv4J1E98cUNdpzF9JIFRPMCNdGOvTr4UB+BhQv9GWg=="
-
-        return Data.init(base64Encoded: base64EncodedKey)!
     }
 }
