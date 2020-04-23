9c6a47b882fe67a00cc75a574b3b8c79ff4f2c39
Robert Atkins <ratkins@pivotal.io>
2020-04-23 09:51:09 +0200

    Add more sophisticated state restoration

--- CoLocate/Bluetooth/BTLEBroadcaster.swift
+++ CoLocate/Bluetooth/BTLEBroadcaster.swift
@@ -24,11 +24,14 @@ class ConcreteBTLEBroadcaster: NSObject, BTLEBroadcaster, CBPeripheralManagerDel
     static let sonarServiceUUID = CBUUID(string: "c1f5983c-fa94-4ac8-8e2e-bb86d6de9b21")
     static let sonarIdCharacteristicUUID = CBUUID(string: "85BF337C-5B64-48EB-A5F7-A9FED135C972")
     static let keepaliveCharacteristicUUID = CBUUID(string: "D802C645-5C7B-40DD-985A-9FBEE05FE85C")
+    
+    let advertismentDataLocalName = "Sonar"
 
     var peripheral: CBPeripheralManager?
     var keepaliveCharacteristic: CBMutableCharacteristic?
     var keepaliveValue: Data?
     var stateDelegate: BTLEBroadcasterStateDelegate?
+    
     let idGenerator: BroadcastIdGenerator
     
     init(idGenerator: BroadcastIdGenerator) {
@@ -89,13 +92,38 @@ class ConcreteBTLEBroadcaster: NSObject, BTLEBroadcaster, CBPeripheralManagerDel
     // MARK: - CBPeripheralManagerDelegate
 
     func peripheralManager(_ peripheral: CBPeripheralManager, willRestoreState dict: [String : Any]) {
-        guard let services = dict[CBPeripheralManagerRestoredStateServicesKey] as? [CBMutableService],
-              let sonarIdService = services.first else {
-            logger.info("No services to restore!")
+        guard let services = dict[CBPeripheralManagerRestoredStateServicesKey] as? [CBMutableService] else {
+            logger.info("no services restored, creating from scratch...")
+            start()
             return
         }
-
-        logger.info("restoring \(sonarIdService)")
+        for service in services {
+            logger.info("restoring service \(service)")
+            guard let characteristics = service.characteristics else {
+                assertionFailure("service has no characteristics, this shouldn't happen")
+                return
+            }
+            for characteristic in characteristics {
+                if characteristic.uuid == ConcreteBTLEBroadcaster.keepaliveCharacteristicUUID {
+                    logger.info("    retaining restored keepalive characteristic \(characteristic)")
+                    self.keepaliveCharacteristic = (characteristic as! CBMutableCharacteristic)
+                } else {
+                    logger.info("    restored characteristic \(characteristic)")
+                }
+            }
+        }
+        if let advertismentData = dict[CBPeripheralManagerRestoredStateAdvertisementDataKey] as? [String: Any] {
+            logger.info("restored advertisementData \(advertismentData)")
+        }
+        if peripheral.isAdvertising {
+            logger.info("peripheral manager is advertising")
+        } else {
+            logger.info("peripheral manager not advertising, starting...")
+            peripheral.startAdvertising([
+                CBAdvertisementDataLocalNameKey: advertismentDataLocalName,
+                CBAdvertisementDataServiceUUIDsKey: [ConcreteBTLEBroadcaster.sonarServiceUUID]
+            ])
+        }
     }
 
     func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
@@ -103,7 +131,7 @@ class ConcreteBTLEBroadcaster: NSObject, BTLEBroadcaster, CBPeripheralManagerDel
         
         stateDelegate?.btleBroadcaster(self, didUpdateState: peripheral.state)
 
-        switch (peripheral.state) {
+        switch peripheral.state {
             
         case .poweredOn:
             self.peripheral = peripheral
@@ -123,7 +151,7 @@ class ConcreteBTLEBroadcaster: NSObject, BTLEBroadcaster, CBPeripheralManagerDel
         logger.info("advertising identifier \(idGenerator.broadcastIdentifier()?.base64EncodedString() ??? "nil")")
         
         peripheral.startAdvertising([
-            CBAdvertisementDataLocalNameKey: "Sonar",
+            CBAdvertisementDataLocalNameKey: advertismentDataLocalName,
             CBAdvertisementDataServiceUUIDsKey: [service.uuid]
         ])
     }
