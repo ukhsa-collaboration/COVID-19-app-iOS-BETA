1c90107756792a44ca51104c6575ed68bc6fdbaa
Anon 22 <anon22@example.com>
2020-04-24 12:14:01 +0100

    Coordinator for main self diagnosis flow

--- CoLocate/SelfDiagnosis/QuestionSymptomsViewController.swift
+++ CoLocate/SelfDiagnosis/QuestionSymptomsViewController.swift
@@ -6,4 +6,65 @@
 //  Copyright © 2020 NHSX. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class QuestionSymptomsViewController: UIViewController, Storyboarded {
+    static var storyboardName = "SelfDiagnosis"
+    
+    @IBOutlet weak var pageLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var detailLabel: UILabel!
+    @IBOutlet weak var errorLabel: UILabel!
+    @IBOutlet weak var yesButton: AnswerButton!
+    @IBOutlet weak var noButton: AnswerButton!
+    @IBOutlet weak var questionButton: PrimaryButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var buttonAction: ((Bool) -> Void)!
+    var questionState: Bool?
+    
+    func inject(
+        pageNumber: Int,
+        pageCount: Int,
+        questionTitle: String,
+        questionDetail: String,
+        questionError: String,
+        questionYes: String,
+        questionNo: String,
+        buttonText: String,
+        buttonAction: @escaping (Bool) -> Void
+    ) {        
+        pageLabel.text = "\(pageNumber)/\(pageCount)"
+        titleLabel.text = questionTitle
+        detailLabel.text = questionDetail
+        errorLabel.text = questionError
+        yesButton.text = questionYes
+        noButton.text = questionNo
+        questionButton.titleLabel?.text = buttonText
+        
+        self.buttonAction = buttonAction
+    }
+    
+    @IBAction func yesTapped(_ sender: Any) {
+        yesButton.isSelected = true
+        noButton.isSelected = false
+        questionState = true
+        scrollView.scrollRectToVisible(questionButton.frame, animated: true)
+    }
+    
+    @IBAction func noTapped(_ sender: Any) {
+        yesButton.isSelected = false
+        noButton.isSelected = true
+        questionState = false
+        scrollView.scrollRectToVisible(questionButton.frame, animated: true)
+    }
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        guard let state = questionState else {
+            errorLabel.isHidden = false
+            return
+        }
+        buttonAction(state)
+    }
+    
+}
---
+++ CoLocate/SelfDiagnosis/SelfDiagnosisCoordinator.swift
@@ -0,0 +1,83 @@
+//
+//  SelfDiagnosisCoordinator.swift
+//  CoLocate
+//
+//  Created by NHSX.
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import UIKit
+
+protocol Coordinator {
+    func start()
+}
+
+class SelfDiagnosisCoordinator: Coordinator {
+    let navigationController: UINavigationController
+    let persisting: Persisting
+    let contactEventRepository: ContactEventRepository
+    let session: Session
+    
+    init(
+        navigationController: UINavigationController,
+        persisting: Persisting,
+        contactEventRepository: ContactEventRepository,
+        session: Session
+    ) {
+        self.navigationController = navigationController
+        self.persisting = persisting
+        self.contactEventRepository = contactEventRepository
+        self.session = session
+    }
+    
+    var hasHighTemperature: Bool!
+    var hasNewCough: Bool!
+    
+    func start() {
+        let vc = QuestionSymptomsViewController.instantiate()
+        vc.inject(
+            pageNumber: 1,
+            pageCount: 3,
+            questionTitle: "TEMPERATURE_QUESTION".localized,
+            questionDetail: "TEMPERATURE_DETAIL".localized,
+            questionError: "TEMPERATURE_ERROR".localized,
+            questionYes: "TEMPERATURE_YES".localized,
+            questionNo: "TEMPERATURE_NO".localized,
+            buttonText: "Continue"
+        ) { hasHighTemperature in
+            self.hasHighTemperature = hasHighTemperature
+            self.openCoughView()
+        }
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func openCoughView() {
+        let vc = QuestionSymptomsViewController.instantiate()
+        vc.inject(
+            pageNumber: 1,
+            pageCount: 3,
+            questionTitle: "COUGH_QUESTION".localized,
+            questionDetail: "COUGH_DETAIL".localized,
+            questionError: "COUGH_ERROR".localized,
+            questionYes: "COUGH_YES".localized,
+            questionNo: "COUGH_NO".localized,
+            buttonText: "Continue"
+        ) { hasNewCough in
+            self.hasNewCough = hasNewCough
+            self.openSubmissionView()
+        }
+        navigationController.pushViewController(vc, animated: true)
+    }
+    
+    func openSubmissionView() {
+        let vc = SubmitSymptomsViewController.instantiate()
+        vc.inject(
+            persisting: persisting,
+            contactEventRepository: contactEventRepository,
+            session: session,
+            hasHighTemperature: hasHighTemperature,
+            hasNewCough: hasNewCough
+        )
+        navigationController.pushViewController(vc, animated: true)
+    }
+}
