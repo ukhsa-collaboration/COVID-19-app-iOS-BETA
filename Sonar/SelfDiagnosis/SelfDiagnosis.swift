df15c2fc81b2cc2d3b3f725fdd28b879ecfabb45
Anon 22 <anon22@example.com>
2020-04-29 15:48:32 +0100

    Adds additional expiry logic to self diagnosis

--- Sonar/SelfDiagnosis/SelfDiagnosis.swift
+++ Sonar/SelfDiagnosis/SelfDiagnosis.swift
@@ -11,12 +11,21 @@ import Foundation
 struct SelfDiagnosis: Codable, Equatable {
     let symptoms: Set<Symptom>
     let startDate: Date
-    let expiryDate: Date
+    var expiryDate: Date
 
     init(symptoms: Set<Symptom>, startDate: Date, expiryDate: Date = Date()) {
         self.symptoms = symptoms
         self.startDate = startDate
         self.expiryDate = expiryDate
+    }
+    
+    mutating func expiresIn(days: Int) {
+        let daysInterval = Double((days - 1) * 24 * 60 * 60)
+        self.expiryDate = Calendar.current.nextDate(
+            after: Date(timeInterval: daysInterval, since: startDate),
+            matching: DateComponents(hour: 7),
+            matchingPolicy: .strict
+        ) ?? Date()
     }
     
     func hasExpired() -> Bool {
