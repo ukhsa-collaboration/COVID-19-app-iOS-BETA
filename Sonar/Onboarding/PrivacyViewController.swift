8ae17cb93299a9373a3fe4d93e7a4b0e886f25ba
Anon 22 <anon22@example.com>
2020-05-01 14:12:16 +0100

    Add link trait to new links in how the app works page

--- Sonar/Onboarding/PrivacyViewController.swift
+++ Sonar/Onboarding/PrivacyViewController.swift
@@ -13,15 +13,20 @@ class PrivacyViewController: UIViewController, Storyboarded {
 
     private var continueHandler: (() -> Void)! = nil
 
-    @IBOutlet weak var informationTitle: UILabel!
     @IBOutlet weak var moreAbout: ButtonWithDynamicType!
     @IBOutlet weak var privacyPolicy: ButtonWithDynamicType!
     @IBOutlet weak var termsConditions: ButtonWithDynamicType!
     
     override func viewDidLoad() {
         moreAbout.setAttributedTitle(NSAttributedString(string: "More about the app".localized, attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue, .foregroundColor: UIColor(named: "NHS Link")!]), for: .normal)
+        moreAbout.accessibilityTraits = .link
+
+        
         privacyPolicy.setAttributedTitle(NSAttributedString(string: "Privacy policy".localized, attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue, .foregroundColor: UIColor(named: "NHS Link")!]), for: .normal)
+        privacyPolicy.accessibilityTraits = .link
+        
         termsConditions.setAttributedTitle(NSAttributedString(string: "Terms and conditions".localized, attributes: [.underlineStyle: NSUnderlineStyle.single.rawValue, .foregroundColor: UIColor(named: "NHS Link")!]), for: .normal)
+        termsConditions.accessibilityTraits = .link
     }
     
     func inject(continueHandler: @escaping () -> Void) {
