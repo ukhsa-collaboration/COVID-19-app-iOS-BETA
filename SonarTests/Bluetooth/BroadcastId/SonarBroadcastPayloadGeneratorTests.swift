63f7a15248c8fb007e24339eb2679b61a13605db
Robert Atkins <ratkins@pivotal.io>
2020-05-12 20:46:05 +0200

    Ensure date calculations are correct around midnight UTC
    [#172619983]

--- SonarTests/Bluetooth/BroadcastId/SonarBroadcastPayloadGeneratorTests.swift
+++ SonarTests/Bluetooth/BroadcastId/SonarBroadcastPayloadGeneratorTests.swift
@@ -27,6 +27,7 @@ class SonarBroadcastPayloadGeneratorTests: XCTestCase {
     var yesterdayMidday: Date!
     var tomorrowMidnightUTC: Date!
     var todayAlmostMidnightUTC: Date!
+    var tomorrowJustAfterMidnightUTC: Date!
 
     override func setUp() {
         now = Date()
@@ -34,6 +35,7 @@ class SonarBroadcastPayloadGeneratorTests: XCTestCase {
         yesterdayMidday = utcCalendar.startOfDay(for: utcCalendar.date(byAdding: .day, value: -1, to: todayMidday)!)
         tomorrowMidnightUTC = utcCalendar.startOfDay(for: utcCalendar.date(byAdding: .day, value: 1, to: now)!)
         todayAlmostMidnightUTC = utcCalendar.date(byAdding: .second, value: -1, to: tomorrowMidnightUTC)
+        tomorrowJustAfterMidnightUTC = utcCalendar.date(byAdding: .second, value: 1, to: tomorrowMidnightUTC)
         
 //        You can test this by setting the TZ variable in the Xcode scheme "Run" configuration to a zone which
 //        is a different day to where you're running ("Pacific/Auckland" is useful, but you've got to be actually
@@ -86,44 +88,64 @@ class SonarBroadcastPayloadGeneratorTests: XCTestCase {
         XCTAssertEqual(storage.savedBroadcastIdDate, todayMidday)
     }
     
-    func test_returns_cached_broadcastId_when_cache_is_fresh() throws {
+    func test_cachedNow_currentDate_almostMidnight_uses_cache() throws {
+        try assertCacheFresh(broadcastIdCreationDate: todayMidday, currentDate: todayAlmostMidnightUTC)
+    }
+    
+    func test_cachedNow_currentDate_now_uses_cache() throws {
+        try assertCacheFresh(broadcastIdCreationDate: todayMidday, currentDate: todayMidday)
+    }
+    
+    func assertCacheFresh(broadcastIdCreationDate: Date, currentDate: Date, file: StaticString = #file, line: UInt = #line) throws {
         let freshBroadcastId = "this is a broadcastId".data(using: .utf8)
 
         storage = MockBroadcastRotationKeyStorage(
             stubbedKey: nil,
             stubbedBroadcastId: freshBroadcastId,
-            stubbedBroadcastDate: todayMidday)
+            stubbedBroadcastDate: broadcastIdCreationDate)
         let persistence = PersistenceDouble(registration: registration)
         generator = SonarBroadcastPayloadGenerator(storage: storage, persistence: persistence, provider: MockEncrypterProvider(encrypter: encrypter))
 
-        let payload = generator.broadcastPayload(date: todayMidday)
+        let payload = generator.broadcastPayload(date: currentDate)
         
-        XCTAssertEqual(encrypter.callCount, 0)
-        XCTAssertEqual(payload?.cryptogram, freshBroadcastId)
-        XCTAssertNil(storage.savedBroadcastId)
-        XCTAssertNil(storage.savedBroadcastIdDate)
+        XCTAssertEqual(encrypter.callCount, 0, file: file, line: line)
+        XCTAssertEqual(payload?.cryptogram, freshBroadcastId, file: file, line: line)
+        XCTAssertNil(storage.savedBroadcastId, file: file, line: line)
+        XCTAssertNil(storage.savedBroadcastIdDate, file: file, line: line)
+    }
+
+    func test_cached_yesterday_currentDate_midday_regenerates_broadcastId() throws {
+        try assertStaleCacheWith(broadcastIdCreationDate: yesterdayMidday, currentDate: todayMidday)
     }
     
-    func test_caches_and_returns_new_broadcastId_when_cache_is_stale() throws {
+    func test_cached_now_currentDate_midnight_regenerates_broadcastId() throws {
+        try assertStaleCacheWith(broadcastIdCreationDate: now, currentDate: tomorrowMidnightUTC)
+    }
+    
+    func test_cached_now_currentDate_just_after_midnight_regenerates_broadcastId() throws {
+        try assertStaleCacheWith(broadcastIdCreationDate: now, currentDate: tomorrowJustAfterMidnightUTC)
+    }
+    
+    func assertStaleCacheWith(broadcastIdCreationDate: Date, currentDate: Date, file: StaticString = #file, line: UInt = #line) throws {
         let staleBroadcastId = "this is a broadcastId".data(using: .utf8)
 
         storage = MockBroadcastRotationKeyStorage(
             stubbedKey: nil,
             stubbedBroadcastId: staleBroadcastId,
-            stubbedBroadcastDate: yesterdayMidday)
+            stubbedBroadcastDate: broadcastIdCreationDate)
         let persistence = PersistenceDouble(registration: registration)
         generator = SonarBroadcastPayloadGenerator(storage: storage, persistence: persistence, provider: MockEncrypterProvider(encrypter: encrypter))
 
-        let payload = generator.broadcastPayload(date: todayMidday)
+        let payload = generator.broadcastPayload(date: currentDate)
         
-        XCTAssertEqual(encrypter.startDate, todayMidday)
-        XCTAssertEqual(encrypter.endDate, now.followingMidnightUTC)
-        XCTAssertEqual(encrypter.callCount, 1)
-        XCTAssertNotEqual(payload?.cryptogram, staleBroadcastId)
-        XCTAssertEqual(storage.savedBroadcastId, MockEncrypter.broadcastId)
-        XCTAssertEqual(storage.savedBroadcastIdDate, todayMidday)
+        XCTAssertEqual(encrypter.startDate, currentDate, file: file, line: line)
+        XCTAssertEqual(encrypter.endDate, currentDate.followingMidnightUTC, file: file, line: line)
+        XCTAssertEqual(encrypter.callCount, 1, file: file, line: line)
+        XCTAssertEqual(payload?.cryptogram, MockEncrypter.broadcastId, file: file, line: line)
+        XCTAssertEqual(storage.savedBroadcastId, MockEncrypter.broadcastId, file: file, line: line)
+        XCTAssertEqual(storage.savedBroadcastIdDate, currentDate, file: file, line: line)
     }
-    
+
 }
 
 class MockBroadcastRotationKeyStorage: BroadcastRotationKeyStorage {
