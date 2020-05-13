23ac030023fde835acafd3d4af2134bf1f72f2d1
Robert Atkins <ratkins@pivotal.io>
2020-05-13 12:42:22 +0200

    Update the broadcast payload on a timer every midnight UTC
    - This ensures already-connected peers receive a new contact event with a new broadcast id every midnight
    - Note we don’t reschedule this timer; we assume this won’t be a problem because it’s unlikely the app will be foregrounded for more than 24h
    [#172693248]

---
+++ Sonar/Extensions/DateExtensions.swift
@@ -0,0 +1,29 @@
+//
+//  DateExtensions.swift
+//  Sonar
+//
+//  Created by NHSX.
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    
+    var midday: Date {
+        return Calendar.current.date(bySettingHour: 12, minute: 0, second: 0, of: self)!
+    }
+
+    var followingMidnightUTC: Date {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+        return calendar.startOfDay(for: calendar.date(byAdding: .day, value: 1, to: self)!)
+    }
+    
+    var precedingMidnightUTC: Date {
+        var calendar = Calendar(identifier: .gregorian)
+        calendar.timeZone = TimeZone(identifier: "UTC")!
+        return calendar.startOfDay(for: self)
+    }
+
+}
