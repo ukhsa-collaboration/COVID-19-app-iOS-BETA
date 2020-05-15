36e809ac628e381956d8ad02629bc5b0f26ef8f7
David Evans <devans@pivotal.io>
2020-05-15 18:43:11 +0100

    use page objects for diagnosis flow tests

--- SonarUITests/Screens/StatusTests.swift
+++ SonarUITests/Screens/StatusTests.swift
@@ -13,94 +13,90 @@ class StatusTests: ScreenTestCase {
     override var screen: Screen { .status }
 
     func testSelfDiagnosisPositiveFlow() {
-        // Blue advice screen
-        XCTAssert(blueAdvice.exists)
+        StatusOkPage(app)
+            .assert { $0.hasStatusOkHeading }
+            .tapFeelUnwell()
 
-        feelUnwellButton.tap()
+            .tapTemperatureOption().tapContinue()
+            .tapCoughOption().tapContinue()
+            .tapStartDateButton().tapContinue()
+            .tapAccurateConfirmationToggle().tapSubmit()
 
-        // Symptom selection screens
-        highTemperatureOption.tap()
-        continueButton.tap()
+            .assert { $0.hasStatusSymptomaticHeading }
+            .assert { $0.hasBookNowAdvice }
 
-        continuousCoughOption.tap()
-        continueButton.tap()
+            // waits for 7 days then starts asking user for updates
+            .eightDaysLater()
 
-        // Start date screen
-        startDateButton.tap()
-        continueButton.tap()
+            .checkinQuestionnairePopup
+            .assert { $0.isShowingCheckinPrompt }
+            .tapUpdateSymptoms()
 
-        // Data submission screen
-        accurateConfirmationToggle.tap()
-        submitButton.tap()
+            // questionnaire reappears if the user cancels
+            .tapCancel()
+            .assert { $0.isShowingCheckinPrompt }
+            .tapUpdateSymptoms()
 
-        // Self diagnosed advice screen
-        XCTAssert(selfDiagnosedAdvice.exists)
-        XCTAssert(bookNowAdvice.exists)
+            // if the user still has symptoms...
+            .tapTemperatureOption().tapContinue()
+            .tapCoughOption().tapSubmit()
 
-        eightDaysLater()
+            // they are told to continue isolating
+            .expectNoAdvicePopup()
+            .assert { $0.hasStatusSymptomaticHeading }
 
-        XCTAssert(questionnairePopup.exists)
-        questionnaireUpdateButton.tap()
-        cancelButton.tap()
+            .eightDaysLater()
 
-        // questionnaire reappears if the user cancels
-        XCTAssert(questionnairePopup.exists)
-        questionnaireUpdateButton.tap()
+            .checkinQuestionnairePopup
+            .assert { $0.isShowingCheckinPrompt }
+            .tapUpdateSymptoms()
 
-        // if the user still has symptoms...
-        highTemperatureOption.tap();
-        continueButton.tap();
-        continuousCoughOption.tap();
-        submitButton.tap();
+            // if the user only has a cough...
+            .tapNoTemperatureOption().tapContinue()
+            .tapCoughOption().tapSubmit()
 
-        // they are told to continue isolating
-        XCTAssert(selfDiagnosedAdvice.exists)
+            // ...they are told to return to "current advice"...
+            .checkAndDismissCoughAdvice()
+            .assert { $0.hasStatusOkHeading }
 
-        eightDaysLater()
+            .eightDaysLater()
 
-        XCTAssert(questionnairePopup.exists)
-        questionnaireUpdateButton.tap()
-
-        // if the user only has a cough...
-        noHighTemperatureOption.tap();
-        continueButton.tap();
-        continuousCoughOption.tap();
-        submitButton.tap();
-
-        // ...they are told to return to "current advice"...
-        XCTAssert(coughAdvice.exists)
-        closeButton.tap();
-
-        eightDaysLater()
-
-        // ...and are not reminded again
-        XCTAssertFalse(questionnairePopup.exists)
+            // ...and are not reminded again
+            .checkinQuestionnairePopup
+            .assert { !$0.isShowingCheckinPrompt }
     }
 
     func testSelfDiagnosisNegativeFlow() {
-        // Blue advice screen
-        XCTAssert(blueAdvice.exists)
+        StatusOkPage(app)
+            .assert { $0.hasStatusOkHeading }
+            .tapFeelUnwell()
 
-        feelUnwellButton.tap()
+            .tapNoTemperatureOption().tapContinue()
+            .tapNoCoughOption().tapContinue()
 
-        // Symptom selection screens
-        noHighTemperatureOption.tap()
-        continueButton.tap()
+            .assert { $0.hasNoSymptoms }
+            .tapDone()
 
-        noContinuousCoughOption.tap()
-        continueButton.tap()
-
-        // No symptom advice screen
-        XCTAssert(noSymptomsAdvice.exists)
-        doneButton.tap()
-
-        // Blue advice screen
-        XCTAssert(blueAdvice.exists)
+            .assert { $0.hasStatusOkHeading }
     }
 }
 
-private extension ScreenTestCase {
-    func eightDaysLater() {
+protocol PageChainable { }
+
+extension PageChainable {
+    @discardableResult
+    func then(block: (Self) -> Void) -> Self {
+        block(self)
+        return self
+    }
+
+    @discardableResult
+    func assert(expression: (Self) -> Bool) -> Self {
+        XCTAssert(expression(self))
+        return self
+    }
+
+    func eightDaysLater() -> Self {
         // close app
         XCUIDevice.shared.press(.home)
 
@@ -109,82 +105,151 @@ private extension ScreenTestCase {
 
         XCUIApplication().activate()
         usleep(500000) // wait for app opening animation
+        return self
+    }
+}
+
+class Page : PageChainable {
+    let app: XCUIApplication
+
+    init(_ app: XCUIApplication) {
+        self.app = app
     }
 
-    var blueAdvice: XCUIElement {
-        app.staticTexts["Follow the current advice to stop the spread of coronavirus"]
+    @discardableResult
+    func tapButton(_ label: String) -> Self {
+        app.buttons[label].tap()
+        return self
+    }
+}
+
+class StatusPage : Page {
+    var checkinQuestionnairePopup: CheckinQuestionnairePopup {
+        CheckinQuestionnairePopup(app)
+    }
+}
+
+class StatusOkPage : StatusPage {
+    var hasStatusOkHeading: Bool {
+        app.staticTexts["Follow the current advice to stop the spread of coronavirus"].exists
     }
 
-    var feelUnwellButton: XCUIElement {
-        app.staticTexts["I feel unwell"]
+    func tapFeelUnwell() -> SymptomsTemperaturePage {
+        app.staticTexts["I feel unwell"].tap()
+        return SymptomsTemperaturePage(app)
+    }
+}
+
+class StatusSymptomaticPage : StatusPage {
+    var hasStatusSymptomaticHeading: Bool {
+        app.staticTexts["Your symptoms indicate you may have coronavirus. Please self-isolate and apply for a test."].exists
     }
 
-    var highTemperatureOption: XCUIElement {
-        app.buttons["Yes, I have a high temperature"]
+    var hasBookNowAdvice: Bool {
+        app.staticTexts["Please book a coronavirus test immediately. Write down your reference code and phone 0800 540 4900"].exists
+    }
+}
+
+class SymptomsTemperaturePage : Page {
+    func tapTemperatureOption() -> Self { return tapButton("Yes, I have a high temperature") }
+
+    func tapNoTemperatureOption() -> Self { return tapButton("No, I do not have a high temperature") }
+
+    func tapContinue() -> SymptomsCoughPage {
+        tapButton("Continue")
+        return SymptomsCoughPage(app)
+    }
+}
+
+class SymptomsCoughPage : Page {
+    func tapCoughOption() -> Self { return tapButton("Yes, I have a new continuous cough") }
+
+    func tapNoCoughOption() -> Self { return tapButton("No, I do not have a new continuous cough") }
+
+    func tapContinue() -> SymptomsAdvicePage {
+        app.buttons["Continue"].tap()
+        return SymptomsAdvicePage(app)
+    }
+}
+
+class SymptomsAdvicePage : Page {
+    var hasNoSymptoms: Bool {
+        app.staticTexts["You do not appear to have coronavirus symptoms"].exists
     }
 
-    var noHighTemperatureOption: XCUIElement {
-        app.buttons["No, I do not have a high temperature"]
+    func tapDone() -> StatusOkPage {
+        tapButton("Done")
+        return StatusOkPage(app)
     }
 
-    var continuousCoughOption: XCUIElement {
-        app.buttons["Yes, I have a new continuous cough"]
+    func tapStartDateButton() -> Self { return tapButton("Select start date") }
+
+    func tapContinue() -> SymptomsSubmitPage {
+        app.buttons["Continue"].tap()
+        return SymptomsSubmitPage(app)
+    }
+}
+
+class SymptomsSubmitPage : Page {
+    func tapAccurateConfirmationToggle() -> Self {
+        app.switches["Please toggle the switch to confirm the information you entered is accurate"].tap()
+        return self
     }
 
-    var noContinuousCoughOption: XCUIElement {
-        app.buttons["No, I do not have a new continuous cough"]
+    func tapSubmit() -> StatusSymptomaticPage {
+        tapButton("Submit")
+        return StatusSymptomaticPage(app)
+    }
+}
+
+class CheckinQuestionnairePopup : Page {
+    var isShowingCheckinPrompt: Bool {
+        app.staticTexts["How are you feeling today?"].exists
     }
 
-    var continueButton: XCUIElement {
-        app.buttons["Continue"]
+    func tapUpdateSymptoms() -> CheckinTemperaturePage {
+        tapButton("Update my symptoms")
+        return CheckinTemperaturePage(app)
+    }
+}
+
+class CheckinPage : Page {
+    func tapCancel() -> CheckinQuestionnairePopup {
+        tapButton("Cancel")
+        return CheckinQuestionnairePopup(app)
+    }
+}
+
+class CheckinTemperaturePage : CheckinPage {
+    func tapTemperatureOption() -> Self { return tapButton("Yes, I have a high temperature") }
+
+    func tapNoTemperatureOption() -> Self { return tapButton("No, I do not have a high temperature") }
+
+    func tapContinue() -> CheckinCoughPage {
+        tapButton("Continue")
+        return CheckinCoughPage(app)
+    }
+}
+
+class CheckinCoughPage : CheckinPage {
+    func tapCoughOption() -> Self { return tapButton("Yes, I have a new continuous cough") }
+
+    func tapNoCoughOption() -> Self { return tapButton("No, I do not have a new continuous cough") }
+
+    func tapSubmit() -> CheckinAdvicePage {
+        tapButton("Submit")
+        return CheckinAdvicePage(app)
+    }
+}
+
+class CheckinAdvicePage : CheckinPage {
+    func expectNoAdvicePopup() -> StatusSymptomaticPage {
+        return StatusSymptomaticPage(app)
     }
 
-    var noSymptomsAdvice: XCUIElement {
-        app.staticTexts["You do not appear to have coronavirus symptoms"]
+    func checkAndDismissCoughAdvice() -> StatusOkPage {
+        XCTAssert(app.staticTexts["Although you still have a continuous cough, you can now follow the current advice."].exists)
+        tapButton("Close")
+        return StatusOkPage(app)
     }
-
-    var doneButton: XCUIElement {
-        app.buttons["Done"]
-    }
-
-    var startDateButton: XCUIElement {
-        app.buttons["Select start date"]
-    }
-
-    var accurateConfirmationToggle: XCUIElement {
-        app.switches["Please toggle the switch to confirm the information you entered is accurate"]
-    }
-
-    var submitButton: XCUIElement {
-        app.buttons["Submit"]
-    }
-
-    var selfDiagnosedAdvice: XCUIElement {
-        app.staticTexts["Your symptoms indicate you may have coronavirus. Please self-isolate and apply for a test."]
-    }
-
-    var bookNowAdvice: XCUIElement {
-        app.staticTexts["Please book a coronavirus test immediately. Write down your reference code and phone 0800 540 4900"]
-    }
-
-    var questionnairePopup: XCUIElement {
-        app.staticTexts["How are you feeling today?"]
-    }
-
-    var questionnaireUpdateButton: XCUIElement {
-        app.buttons["Update my symptoms"]
-    }
-
-    var cancelButton: XCUIElement {
-        app.buttons["Cancel"]
-    }
-
-    var coughAdvice: XCUIElement {
-        app.staticTexts["Although you still have a continuous cough, you can now follow the current advice."]
-    }
-
-    var closeButton: XCUIElement {
-        app.buttons["Close"]
-    }
-
 }
