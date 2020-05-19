9b9d19f1e3c6f3ffb54d1d9c62ad0d0fae2386d4
Alpha Chen <achen@pivotal.io>
2020-05-19 11:34:10 -0700

    Implement a Symptoms stuct that is backwards compatible with Set<Symptom>

--- Sonar/SelfDiagnosis/Symptoms.swift
+++ Sonar/SelfDiagnosis/Symptoms.swift
@@ -11,3 +11,49 @@ import Foundation
 enum Symptom: String, Codable {
     case temperature, cough, smellLoss, fever, nausea
 }
+
+struct Symptoms: Equatable {
+    private var symptoms: Set<Symptom>
+
+    var isEmpty: Bool { symptoms.isEmpty }
+
+    init(_ symptoms: Set<Symptom>) {
+        self.symptoms = symptoms
+    }
+
+    func contains(_ symptom: Symptom) -> Bool {
+        return symptoms.contains(symptom)
+    }
+
+    mutating func insert(_ symptom: Symptom) {
+        symptoms.insert(symptom)
+    }
+}
+
+extension Symptoms: ExpressibleByArrayLiteral {
+    typealias ArrayLiteralElement = Symptom
+
+    init(arrayLiteral elements: Symptom...) {
+        symptoms = Set(elements)
+    }
+}
+
+extension Symptoms: Encodable {
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.unkeyedContainer()
+        try container.encode(contentsOf: symptoms)
+    }
+}
+
+extension Symptoms: Decodable {
+    init(from decoder: Decoder) throws {
+        var container = try decoder.unkeyedContainer()
+
+        var symptoms: Set<Symptom> = []
+        while !container.isAtEnd {
+            symptoms.insert(try container.decode(Symptom.self))
+        }
+
+        self.symptoms = symptoms
+    }
+}
