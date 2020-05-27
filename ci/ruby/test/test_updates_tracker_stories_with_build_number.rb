13bc92e20c539844e9a0061d9081e6e163919a99
Tom Brand <tbrand@pivotal.io>
2020-05-27 12:43:41 +0100

    [#172789460] Improve robustness of update tracker story script

--- ci/ruby/test/test_updates_tracker_stories_with_build_number.rb
+++ ci/ruby/test/test_updates_tracker_stories_with_build_number.rb
@@ -40,6 +40,7 @@ class TestUpdatesTrackerStoryWithBuildNumber < MiniTest::Test
     git_empty_commit('first commit')
 
     stub_request(:any, %r{https://www.pivotaltracker.com.*})
+    stub_me_request_200
   end
 
   def test_updates_no_stories_when_no_tracker_ids_committed
@@ -52,6 +53,23 @@ class TestUpdatesTrackerStoryWithBuildNumber < MiniTest::Test
 
     assert_not_requested(:any, %r{https://www.pivotaltracker.com.*})
   end
+
+  def test_fails_when_not_authenticated
+    story_id = 1234
+    git_empty_commit("[Finishes ##{story_id}] second commit")
+
+    stub_me_request_401
+
+    assert_raises(RuntimeError, 'unable to authenticate with pivotal tracker') do
+      update_stories_with_build_number(
+        commits: 'HEAD',
+        build_number: build_number,
+        tracker_token: tracker_token,
+        git_dir: tmpdir
+      )
+    end
+  end
+
 
   def test_updates_stories_for_between_given_refs
     project_id = 9876
@@ -76,6 +94,75 @@ class TestUpdatesTrackerStoryWithBuildNumber < MiniTest::Test
       message: "This was delivered in TestFlight build #{build_number}"
     )
   end
+
+  def test_warns_for_unknown_story
+    project_id = 9876
+    story_1_id = 1234
+    story_2_id = 2345
+
+    git_empty_commit("[Finishes ##{story_1_id}] second commit")
+    git_empty_commit("[Finishes ##{story_2_id}] another commit")
+
+    stub_get_story_request_404(story_id: story_1_id)
+    stub_get_story_request(story_id: story_2_id, project_id: project_id)
+
+    stderr = capture_stderr do
+      update_stories_with_build_number(
+        commits: 'HEAD',
+        build_number: build_number,
+        tracker_token: tracker_token,
+        message_template: "This was delivered in TestFlight build %<build_number>s",
+        git_dir: tmpdir
+      )
+    end
+
+    assert_equal("could not find tracker story with id 1234\n", stderr)
+
+    assert_create_story_comment_not_requested(
+      story_id: story_1_id,
+    )
+
+    assert_create_story_comment_requested(
+      project_id: project_id,
+      story_id: story_2_id,
+      message: "This was delivered in TestFlight build #{build_number}"
+    )
+  end
+
+  def test_warns_when_not_authorized_to_read_story
+    project_id = 9876
+    story_1_id = 1234
+    story_2_id = 2345
+
+    git_empty_commit("[Finishes ##{story_1_id}] second commit")
+    git_empty_commit("[Finishes ##{story_2_id}] another commit")
+
+    stub_get_story_request_403(story_id: story_1_id)
+    stub_get_story_request(story_id: story_2_id, project_id: project_id)
+
+    stderr = capture_stderr do
+      update_stories_with_build_number(
+        commits: 'HEAD',
+        build_number: build_number,
+        tracker_token: tracker_token,
+        message_template: "This was delivered in TestFlight build %<build_number>s",
+        git_dir: tmpdir
+      )
+    end
+
+    assert_equal("not authorized to get story 1234\n", stderr)
+
+    assert_create_story_comment_not_requested(
+      story_id: story_1_id,
+    )
+
+    assert_create_story_comment_requested(
+      project_id: project_id,
+      story_id: story_2_id,
+      message: "This was delivered in TestFlight build #{build_number}"
+    )
+  end
+
 
   def test_updates_stories_for_multiple_tracker_projects
     project_1_id = 9876
@@ -235,12 +322,58 @@ class TestUpdatesTrackerStoryWithBuildNumber < MiniTest::Test
     )
   end
 
+  private def assert_create_story_comment_not_requested(story_id:)
+    assert_not_requested(
+      :post,
+      %r{#{TRACKER_API}/projects/\d+/stories/#{story_id}/comments},
+      headers: stub_headers
+    )
+  end
+
+  private def _stub_me_request
+    stub_request(
+      :get,
+      "#{TRACKER_API}/me"
+    ).with(
+      headers: stub_headers
+    )
+  end
+
+  private def stub_me_request_200
+    _stub_me_request.to_return(
+      status: 200,
+    )
+  end
+
+  private def stub_me_request_401
+    _stub_me_request.to_return(
+      status: 401,
+    )
+  end
+
   private def stub_get_story_request(
     project_id:,
     story_id:
   )
     _stub_get_story_request(story_id).to_return(
+      status: 200,
       body: { :project_id => project_id }.to_json
+    )
+  end
+
+  private def stub_get_story_request_404(
+    story_id:
+  )
+    _stub_get_story_request(story_id).to_return(
+      status: 404,
+    )
+  end
+
+  private def stub_get_story_request_403(
+    story_id:
+  )
+    _stub_get_story_request(story_id).to_return(
+      status: 403,
     )
   end
 
@@ -251,5 +384,27 @@ class TestUpdatesTrackerStoryWithBuildNumber < MiniTest::Test
     ).with(
       headers: stub_headers
     )
+  end
+
+  private def capture_stderr(&block)
+    original_stderr = $stderr
+    $stderr = fake = StringIO.new
+    begin
+      yield
+    ensure
+      $stderr = original_stderr
+    end
+    fake.string
+  end
+
+  private def capture_stdout(&block)
+    original_stdout = $stdout
+    $stdout = fake = StringIO.new
+    begin
+      yield
+    ensure
+      $stdout = original_stdout
+    end
+    fake.string
   end
 end
