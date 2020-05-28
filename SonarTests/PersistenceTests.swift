d7804fce5054a50baf07822f05bb5a9e197c15e0
Alpha Chen <achen@pivotal.io>
2020-05-27 21:00:53 -0700

    Remove a possible source of test flakiness

--- SonarTests/PersistenceTests.swift
+++ SonarTests/PersistenceTests.swift
@@ -223,8 +223,9 @@ class PersistenceTests: TestCase {
     }
 
     func testStatusStateMigration() throws {
-        let expiryDate = Calendar.current.date(byAdding: .day, value: 1, to: Date())!
-        let diagnosis = SelfDiagnosis(type: .initial, symptoms: [.cough], startDate: Date(), expiryDate: expiryDate)
+        let startDate = Date()
+        let expiryDate = Calendar.current.date(byAdding: .day, value: 1, to: startDate)!
+        let diagnosis = SelfDiagnosis(type: .initial, symptoms: [.cough], startDate: startDate, expiryDate: expiryDate)
         let encoded = try encoder.encode(diagnosis)
         UserDefaults.standard.set(encoded, forKey: "selfDiagnosis")
 
