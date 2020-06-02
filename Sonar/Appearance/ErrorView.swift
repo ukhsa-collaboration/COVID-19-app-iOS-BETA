e96d44e76276fee55f8a84c57ec02f6196f0c7de
Anon 12 <anon12@example.com>
2020-06-02 12:24:22 -0700

    New look for error messsages in normal mode
    [#173060307]

--- Sonar/Appearance/ErrorView.swift
+++ Sonar/Appearance/ErrorView.swift
@@ -10,24 +10,13 @@ import UIKit
 
 
 class ErrorView: IBView {
-    let colour = UIColor(named: "NHS Error")!
     
     @IBOutlet weak var title: UILabel! 
     @IBOutlet weak var errorMessage: AccessibleErrorLabel!
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        
-        title.text = "There is a problem"
-        title.textColor = UIColor(named: "NHS Error")
-
-        errorMessage.text = "Something is incorrect"
         errorMessage.textColor = UIColor(named: "NHS Error")
-
-        layer.borderWidth = 3
-        layer.borderColor = colour.cgColor
-
-        backgroundColor = .clear
     }
     
     override var isHidden: Bool {
