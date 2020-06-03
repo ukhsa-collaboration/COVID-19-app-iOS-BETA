b99b47339a3eb1c9e82a10cb59a0a1c0184945b3
Tom Brand <tbrand@pivotal.io>
2020-06-03 17:08:58 +0100

    Remove ruby ci tools
    Moved to nhsx/sonar-ci-tools
    
    [#172789460]
    
    Signed-off-by: Anon 9 <anon9@example.com>

--- ci/ruby/test/test_helper.rb
+++
@@ -1,7 +0,0 @@
-require 'minitest/autorun'
-
-require 'minitest/reporters'
-Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
-
-require 'webmock/minitest'
-WebMock.disable_net_connect!
