67b6b4f6a769df6456b28a6066c1edbbadb3985c
Anon 12 <anon12@example.com>
2020-06-09 08:40:29 -0700

    Initial RegistrationCanary app shell

---
+++ RegistrationCanary/AppDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.swift
+//  RegistrationCanary
+//
+//  Created by NHSX on 6/9/20
+//  Copyright © 2020 NHSX. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
+
